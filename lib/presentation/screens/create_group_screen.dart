af59e2794d55fa3d7326ea73be5295f4e7a5b23f
tooskroman <sasharyba17@gmail.com>
2023-11-26 21:45:52 +0300

    add: student list fill/delete | animation

--- lib/presentation/screens/create_group_screen.dart
+++ lib/presentation/screens/create_group_screen.dart
@@ -1,3 +1,4 @@
+import 'package:drift/drift.dart' as drift;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
@@ -45,6 +46,7 @@ class _CreateGroupScreenState extends State<CreateGroupScreen> {
                 ],
               )));
     }
+
     return Scaffold(
         appBar: AppBar(
           leading: Builder(
@@ -99,46 +101,7 @@ class _CreateGroupScreenState extends State<CreateGroupScreen> {
                   ],
                 ),
                 const Gap(16),
-                ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: students.length + 1,
-                    itemBuilder: (context, count) {
-                      if (count == 0) {
-                        return InkWell(
-                          onTap: () {},
-                          child: ListTile(
-                            leading: const Icon(
-                              Icons.add_outlined,
-                              color: Colors.white,
-                            ),
-                            title: Text("Добавить студента",
-                                style: Theme.of(context).textTheme.bodyMedium),
-                          ),
-                        );
-                      } else {
-                        return ListTile(
-                          //TODO: create component with outer functions
-                          leading: Text(count.toString()),
-                          title: Row(children: [
-                            Expanded(
-                              child: TextFormField(
-                                initialValue: students[count - 1].name,
-                                onChanged: (value) {},
-                              ),
-                            ),
-                            const Gap(16),
-                            IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.delete_outline))
-                          ]),
-                          trailing: Switch(
-                              value: students[count - 1].isAdmin ?? false,
-                              onChanged: (value) => setState(() {
-                                    //  TODO: update in db
-                                  })),
-                        );
-                      }
-                    }),
+                StudentInfoList(students),
                 const Gap(16),
                 Text("Добавьте занятия",
                     style: Theme.of(context).textTheme.headlineSmall),
@@ -170,5 +133,148 @@ class _CreateGroupScreenState extends State<CreateGroupScreen> {
             ),
           ),
         ));
+  }
+}
+
+class StudentInfoList extends StatelessWidget {
+  StudentInfoList(
+    this.students, {
+    super.key,
+  });
+
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
+  final List<Student> students;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedList(
+        key: _listKey,
+        shrinkWrap: true,
+        initialItemCount: students.length + 1,
+        itemBuilder: (context, count, animation) {
+          if (count == students.length) {
+            return AddNewStudentTile(animation, _listKey, students);
+          } else {
+            return StudentInfoTile(animation, _listKey, students, count);
+          }
+        });
+  }
+}
+
+class AddNewStudentTile extends StatelessWidget {
+  final Animation<double> animation;
+  final GlobalKey<AnimatedListState> _listKey;
+  final List<Student> students;
+  const AddNewStudentTile(this.animation, this._listKey, this.students,
+      {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+      sizeFactor: animation.drive(CurveTween(curve: Curves.easeInOut)),
+      child: InkWell(
+        onTap: () {
+          _onAddButtonPressed(_listKey);
+        },
+        child: ListTile(
+          // Create new student field
+          leading: const Icon(
+            Icons.add_outlined,
+            color: Colors.white,
+          ),
+          title: Text("Добавить студента",
+              style: Theme.of(context).textTheme.bodyMedium),
+        ),
+      ),
+    );
+  }
+
+  void _onAddButtonPressed(GlobalKey<AnimatedListState> listKey) {
+    listKey.currentState?.insertItem(
+      students.length,
+      duration: const Duration(milliseconds: 500),
+    );
+    students.add(Student(id: students.length + 1, name: '', isAdmin: false));
+  }
+}
+
+class StudentInfoTile extends StatefulWidget {
+  final Animation<double> animation;
+  final GlobalKey<AnimatedListState> _listKey;
+  final List<Student> students;
+  final int count;
+  const StudentInfoTile(
+      this.animation, this._listKey, this.students, this.count,
+      {super.key});
+
+  @override
+  State<StudentInfoTile> createState() => _StudentInfoTileState();
+}
+
+class _StudentInfoTileState extends State<StudentInfoTile> {
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+        key: ValueKey(widget.students[widget.count]),
+        sizeFactor: widget.animation,
+        child: ListTile(
+          // Student field
+          //TODO: create component with outer functions
+          leading: Text((widget.count + 1).toString()),
+          title: Row(children: [
+            Expanded(
+                child: TextFormField(
+              focusNode: (widget.count == widget.students.length - 1 &&
+                      widget.students[widget.count].name.isEmpty)
+                  ? (FocusNode()..requestFocus())
+                  : null,
+              key: UniqueKey(),
+              initialValue: widget.students[widget.count].name,
+              onChanged: (value) {
+                widget.students[widget.count] =
+                    widget.students[widget.count].copyWith(name: value);
+              },
+              autovalidateMode: AutovalidateMode.always,
+              validator: (value) =>
+                  value?.isEmpty ?? true ? "Необходимо заполнить поле" : null,
+            )),
+            const Gap(16),
+            IconButton(
+                onPressed: () {
+                  _onDeleteButtonPressed(
+                      context, widget.count, widget._listKey);
+                },
+                icon: const Icon(Icons.delete_outline))
+          ]),
+          trailing: Switch(
+              value: widget.students[widget.count].isAdmin ?? false,
+              onChanged: (value) {
+                setState(() {
+                  widget.students[widget.count] = widget.students[widget.count]
+                      .copyWith(isAdmin: drift.Value(value));
+                });
+              }),
+        ));
+  }
+
+  void _onDeleteButtonPressed(
+      BuildContext context, int count, GlobalKey<AnimatedListState> listKey) {
+    listKey.currentState?.removeItem(
+        count,
+        (count, a) => SizeTransition(
+              sizeFactor: a.drive(CurveTween(curve: Curves.easeInOut)),
+              child: ListTile(
+                title: Align(
+                    child: Icon(
+                  Icons.delete_outline_outlined,
+                  color: Theme.of(context).colorScheme.error,
+                )),
+              ),
+            ),
+        duration: const Duration(milliseconds: 500));
+    widget.students.removeAt(count);
+    for (int i = count; i < widget.students.length; i++) {
+      widget.students[i] = widget.students[i].copyWith(id: i + 1);
+    }
   }
 }
